4ac467ecc36f3935df08404f1fb84c100831650b
nitinlondhe <nitinlondhe2113@gmail.com>
2024-01-16 00:06:45 +0530

    create DownloadRequestQueue

---
+++ library/src/main/java/com/nitinlondhe/library/internal/DownloadRequestQueue.kt
@@ -0,0 +1,45 @@
+package com.nitinlondhe.library.internal
+
+class DownloadRequestQueue(private val dispatcher: DownloadDispatcher) {
+
+    private val idRequestMap: HashMap<Int, DownloadRequest> = hashMapOf()
+
+    fun enqueue(request: DownloadRequest): Int {
+        idRequestMap[request.downloadId] = request
+        return dispatcher.enqueue(request)
+    }
+
+    fun pause(id: Int) {
+        idRequestMap[id]?.let {
+            dispatcher.cancel(it)
+        }
+    }
+
+    fun resume(id: Int) {
+        idRequestMap[id]?.let {
+            dispatcher.enqueue(it)
+        }
+    }
+
+    fun cancel(id: Int) {
+        idRequestMap[id]?.let {
+            dispatcher.cancel(it)
+        }
+        idRequestMap.remove(id)
+    }
+
+    fun cancel(tag: String) {
+        val requestsWithTag = idRequestMap.values.filter {
+            it.tag == tag
+        }
+        for (req in requestsWithTag) {
+            cancel(req.downloadId)
+        }
+    }
+
+    fun cancelAll() {
+        idRequestMap.clear()
+        dispatcher.cancelAll()
+    }
+
+}
